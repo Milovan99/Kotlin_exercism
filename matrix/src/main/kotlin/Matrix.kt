624730cc6396d0c8cb1db1f1ee71a33faa85d292
Milovan99 <milovanjakovljevic78@gmail.com>
2022-12-04 13:19:15 +0100

    Matrix

--- matrix/src/main/kotlin/Matrix.kt
+++ matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,20 @@
 class Matrix(private val matrixAsString: String) {
 
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        var noviRed= matrixAsString.split('\n').toList()
+        var kolona= mutableListOf<Int>()
+        for(i in noviRed.indices){
+            var rezultat=noviRed[i]
+            var rezultatInt =rezultat.split(' ').map { it.toInt() }
+             kolona.add(rezultatInt[colNr-1])
+        }
+        return kolona
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+       var noviRed= matrixAsString.split('\n').toList()
+        var rezultat=noviRed[rowNr-1]
+        var rezultatInt =rezultat.split(' ').map { it.toInt() }
+        return rezultatInt
     }
 }
